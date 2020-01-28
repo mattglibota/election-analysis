f316aa5ae837eef462a800cd0cda3afa87a29972
mattglibota <59847215+mattglibota@users.noreply.github.com>
2020-01-27 19:55:29 -0800

    commit before starting the election lessons

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+#Data we need to retreive
+#1. Total number of votes cast
+#2. complete list of candidates who received votes
+#3. percentage of votes each candidate won
+#4. total number of votes each candidate won
+#5. the winner of election based on popular vote
